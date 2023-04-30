8c7d54c47bfc4b1c1731e4f5a71b60f5dee48ec3
ghaliadirja <113493556+ghaliadirja@users.noreply.github.com>
2023-04-30 18:57:02 +0700

    tugas

---
+++ Instagram/src/ConstData.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Teddy", username: "Michaelteddyyyy", gender: "M", is_view: true, cap: "Apa ya"},
+    { id: 2, name: "Angga", username: "oscar_oasis", gender: "M", is_view: true, cap: "Senyum Palsu" },
+    { id: 3, name: "BagasAji", username: "reallgood ", gender: "M", is_view: true, cap: "Iya kah"},
+    { id: 4, name: "Subhan", username: "kevinn", gender: "M", is_view: true, cap: "Apa kabar saudara saudari" },
+    { id: 5, name: "Basor", username: "Bazzlightyear", gender: "M", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 6, name: "Kevina", username: "acililul", gender: "F", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 7, name: "Aflan", username: "alfan", gender: "M", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 8, name: "Frila", username: "mbaila", gender: "F", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 9, name: "Erdiana", username: "erdiananana", gender: "F", is_view: true, cap: "Mau kemana adik adik" },
+    { id: 10, name: "Febri", username: "febrydfairuz", gender: "M", is_view: true, cap: "Mau kemana adik adik" }
+];
+
+export { UserObj };
